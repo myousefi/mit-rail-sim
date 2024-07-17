5f8da7add9c3e203369a325bf7a2f98ff55cd135
Mojtaba Yousefi <mysfi@outlook.com>
2024-07-17 05:53:56 -0400

    Update TrainHeadwayRegulatorWithEstimatedLoads class in train_headway_regulator.py

--- mit_rail_sim/simulation_engine/train/train_headway_regulator.py
+++ mit_rail_sim/simulation_engine/train/train_headway_regulator.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
 from calendar import c
+from turtle import st
 from typing import TYPE_CHECKING
+
+from mit_rail_sim.simulation_engine.infrastructure import station
+from mit_rail_sim.simulation_engine.passenger import arrival_rate
 
 if TYPE_CHECKING:
     from mit_rail_sim.simulation_engine.infrastructure import Path
@@ -189,6 +193,22 @@ class TrainHeadwayRegulatorWithLoadBalancingAndExactKnowledge(
             following_train.passenger_manager.total_passengers if following_train else 0
         )
 
+        stations_ahead_of_following_train = following_train.get_all_stops_ahead()
+
+        for station in stations_ahead_of_following_train:
+            if station == current_station:
+                break
+            following_train_load += (
+                arrival_rate.get_lambda_for_station(
+                    current_hour=current_hour,
+                    current_weekday=current_weekday,
+                    station=station.name,
+                    direction=direction,
+                )
+                * following_headway
+                // 3600
+            )
+
         target_headway = (
             passenger_retention_rate * (following_train_load - current_train_load)
         ) / (2 * weighted_avg_arrival_rate) + sum_headways / 2
@@ -198,3 +218,125 @@ class TrainHeadwayRegulatorWithLoadBalancingAndExactKnowledge(
         else:
             holding_time = min(target_headway - current_headway, self.max_holding)
             return max(holding_time, self.min_holding)
+
+
+class TrainHeadwayRegulatorWithEstimatedLoads(TrainHeadwayRegulatorAtStation):
+    def __init__(self, critical_station_name: str, max_holding=120, min_holding=30):
+        super().__init__(max_holding, min_holding)
+        self.critical_station_name = critical_station_name
+
+    def suggested_holding(self, train: Train) -> float:
+        current_station = train.current_block.station
+        direction = train.path.direction
+        current_hour = train.simulation.get_current_hour()
+        current_weekday = train.simulation.is_weekday()
+
+        arrival_rate = train.current_block.station.passenger_generator.arrival_rate
+
+        weighted_avg_arrival_rate = arrival_rate.get_lambda_bar(
+            current_hour,
+            current_weekday,
+            current_station.name,
+            self.critical_station_name,
+            direction,
+        )
+
+        weighted_avg_arrival_rate = (
+            weighted_avg_arrival_rate / 3600
+        )  # convert to per second
+
+        passenger_retention_rate = arrival_rate.get_a_i(
+            current_hour,
+            current_weekday,
+            current_station.name,
+            self.critical_station_name,
+            direction,
+        )
+
+        current_headway = (
+            train.simulation.current_time - train.next_block.last_train_visit_time
+        )
+
+        leading_train_id = train.next_block.id_of_last_train
+
+        following_train = train.path.get_previous_train(train.current_block_index)
+
+        if following_train:
+            if following_train.next_block.id_of_last_train == train.train_id:
+                following_headway = (
+                    train.simulation.current_time
+                    - following_train.next_block.last_train_visit_time
+                )
+            elif following_train.next_block.id_of_last_train == leading_train_id:
+                following_headway = (
+                    train.simulation.current_time
+                    - following_train.next_block.last_train_visit_time
+                    - current_headway
+                )
+            else:
+                print(
+                    f"Train sequence error for train {train.train_id} at block {train.next_block.block_alt_name} at time {train.simulation.current_time}"
+                )
+                return 0
+        else:
+            print(
+                f"Follwing train not found for train {train.train_id} at block {train.next_block.block_alt_name} at time {train.simulation.current_time} holding time set to max: {self.max_holding}"
+            )
+            return self.max_holding
+
+        sum_headways = current_headway + following_headway
+
+        L_0_n = 0  # Assuming current train load at current station is 0
+        L_0_n_plus_1 = self.estimate_following_train_load(
+            train, following_train_headway=following_headway
+        )
+
+        target_headway = (passenger_retention_rate * (L_0_n_plus_1 - L_0_n)) / (
+            2 * weighted_avg_arrival_rate
+        ) + sum_headways / 2
+
+        if target_headway <= current_headway:
+            return 0  # Dispatch immediately
+        else:
+            holding_time = min(target_headway - current_headway, self.max_holding)
+            return max(holding_time, self.min_holding)
+
+    def calculate_following_headway(
+        self, train: Train, following_train: Train
+    ) -> float:
+        # Implement the logic to calculate the following headway
+        pass
+
+    def estimate_following_train_load(
+        self, train: Train, following_train_headway: float
+    ) -> float:
+        current_station = train.current_block.station
+        direction = train.path.direction
+
+        arrival_rate = train.current_block.station.passenger_generator.arrival_rate
+
+        stations = arrival_rate.get_all_previous_stops_for_station_and_direction(
+            station=current_station.name, direction=direction
+        )
+
+        L_0_n_plus_1 = 0
+
+        for station in stations:
+            L_0_n_plus_1 += (
+                arrival_rate.get_lambda_for_station(
+                    current_hour=train.simulation.get_current_hour(),
+                    current_weekday=train.simulation.is_weekday(),
+                    station=station,
+                    direction=direction,
+                )
+                * following_train_headway
+                // 3600
+            ) * arrival_rate.get_a_i(
+                current_hour=train.simulation.get_current_hour(),
+                current_weekday=train.simulation.is_weekday(),
+                start_station=station,
+                critical_station=current_station.name,
+                direction=direction,
+            )
+
+        return L_0_n_plus_1
