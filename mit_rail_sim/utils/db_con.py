2c8b13d8de4e42fa0fc3ba988da7d95ed24262c7
Mojtaba Yousefi <mojtaba.yousefi@live.com>
2024-08-14 11:09:30 -0400

    Fix database connection issue and provide warning message

--- mit_rail_sim/utils/db_con.py
+++ mit_rail_sim/utils/db_con.py
@@ -1,14 +1,28 @@
 import os
-
+import warnings
 from dotenv import find_dotenv, load_dotenv
 from sqlalchemy import create_engine, text
 
-load_dotenv(find_dotenv())
 
-USERNAME = os.getenv("USERNAME")
-PASSWORD = os.getenv("PASSWORD")
-HOST = os.getenv("HOST")
-PORT = os.getenv("PORT")
-DATABASE = os.getenv("DATABASE")
+class DummyEngine:
+    def connect(self):
+        raise ConnectionError("Database connection is not available.")
 
-engine = create_engine(f"postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+
+try:
+    load_dotenv(find_dotenv(raise_error_if_not_found=True))
+
+    USERNAME = os.environ["USERNAME"]
+    PASSWORD = os.environ["PASSWORD"]
+    HOST = os.environ["HOST"]
+    PORT = os.environ["PORT"]
+    DATABASE = os.environ["DATABASE"]
+
+    engine = create_engine(
+        f"postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
+    )
+except (FileNotFoundError, KeyError, ValueError):
+    warnings.warn(
+        "Database connection is not available. Some features like CTA String Chart dashboards will not work. Please provide a .env file with correct credentials."
+    )
+    engine = DummyEngine()
